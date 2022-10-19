760ed23ecf9ed39a03cb36d9abf28750c0870dbc
Giulliano Guimarães <llthguimaraes@gmail.com>
2022-10-19 16:54:43 -0300

    Ajuste no menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,57 +6,71 @@ const menuDropDown = document.querySelectorAll(".j_dropdown");
 
 let headerHeight = document.querySelector(".j_main_header").offsetHeight;
 
+const toggleMobileMenu = () => {
+    menuHambIcon.style.height = `${headerHeight}px`;
+    menuHambIcon.classList.toggle("active");
+
+    if (menuHambIcon.classList.contains("active")) {
+        menuHambIcon.querySelector("i").classList.add("fa-xmark");
+        menuHambIcon.querySelector("i").classList.remove("fa-bars");
+
+        fadeIn(menuArea);
+        menuArea.querySelector(".menu").style.left = "0";
+    } else {
+        menuHambIcon.querySelector("i").classList.remove("fa-xmark");
+        menuHambIcon.querySelector("i").classList.add("fa-bars");
+
+        fadeOut(menuArea);
+        menuArea.querySelector(".menu").style.left = "";
+    }
+}
+
+const dropDownMobile = () => {
+    menuDropDown.forEach((item) => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            let submenu = item.querySelector(".submenu");
+
+            if (window.getComputedStyle(submenu).height === "0px") {
+                submenu.style.display = "block";
+                submenu.style.height = "auto";
+            } else {
+                submenu.style.height = "";
+                submenu.style.display = "";
+            }
+        })
+    })
+}
+
+const resetMenu = () => {
+    if (window.innerWidth > 890) {
+        menuArea.style.display = "block";
+        menuArea.style.opacity = "1";
+        menuHambIcon.querySelector("i").classList.remove("fa-xmark");
+        menuHambIcon.querySelector("i").classList.add("fa-bars");
+        menuHambIcon.classList.remove("active");
+    } else {
+        if (!menuHambIcon.classList.contains("active")) {
+            menuArea.style.display = "";
+            menuArea.style.opacity = "";
+        }
+    }
+}
+
 const mobileMenu = () => {
 
-    menuHambIcon.addEventListener("click", () => {
-        menuHambIcon.style.height = `${headerHeight}px`;
-        menuHambIcon.classList.toggle("active");
+    menuHambIcon.addEventListener("click", toggleMobileMenu);
 
-        if (menuHambIcon.classList.contains("active")) {
-            menuHambIcon.querySelector("i").classList.add("fa-xmark");
-            menuHambIcon.querySelector("i").classList.remove("fa-bars");
-
-            fadeIn(menuArea);
-            menuArea.querySelector(".menu").style.left = "0";
-        } else {
-            menuHambIcon.querySelector("i").classList.remove("fa-xmark");
-            menuHambIcon.querySelector("i").classList.add("fa-bars");
-
-            fadeOut(menuArea);
-            menuArea.querySelector(".menu").style.left = "";
-        }
-    })
+    if (window.innerWidth <= 890) {
+        dropDownMobile();
+    }
 
     window.onresize = () => {
-        if (window.innerWidth > 890) {
-            menuArea.style.display = "block";
-            menuArea.style.opacity = "1";
-            menuHambIcon.querySelector("i").classList.remove("fa-xmark");
-            menuHambIcon.querySelector("i").classList.add("fa-bars");
-            menuHambIcon.classList.remove("active");
-        } else {
-            if (!menuHambIcon.classList.contains("active")) {
-                menuArea.style.display = "";
-                menuArea.style.opacity = "";
-            }
-        }
+        resetMenu();
 
         if (window.innerWidth <= 890) {
-            menuDropDown.forEach((item) => {
-                item.addEventListener("click", (event) => {
-                    event.preventDefault();
-    
-                    let submenu = item.querySelector(".submenu");
-    
-                    if (window.getComputedStyle(submenu).height === "0px") {
-                        submenu.style.display = "block";
-                        submenu.style.height = "auto";
-                    } else {
-                        submenu.style.height = "";
-                        submenu.style.display = "";
-                    }
-                })
-            })
+            dropDownMobile();
         }
     }
 }
